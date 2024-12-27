197b1b4c0689e03310e093a07abe9651f2136e35
Euler <huhaiyang7788@126.com>
2024-12-27 21:14:38 +0800

    测试name

--- src/main/java/com/ocean/springbootstart/SpringbootStartApplication.java
+++ src/main/java/com/ocean/springbootstart/SpringbootStartApplication.java
@@ -9,6 +9,7 @@ public class SpringbootStartApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStartApplication.class, args);
         System.out.println("启动成功");
+        // w
     }
 
 }
