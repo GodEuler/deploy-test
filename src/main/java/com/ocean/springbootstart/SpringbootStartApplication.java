ee8bc4bd005eeb56392633e8c0fe7f8c1051e209
euler <huhaiyang7788@126.com>
2024-12-27 21:13:27 +0800

    测试邮箱

--- src/main/java/com/ocean/springbootstart/SpringbootStartApplication.java
+++ src/main/java/com/ocean/springbootstart/SpringbootStartApplication.java
@@ -8,6 +8,7 @@ public class SpringbootStartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootStartApplication.class, args);
+        System.out.println("启动成功");
     }
 
 }
